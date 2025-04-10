b721412f80039a8a53fd75788012f49bcc413cc5
Sophia-Hammond <beautyinbooty@gmail.com>
2025-04-11 00:19:16 +0100

    app.js set up

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require('express-session');
+const session = require('express-session'); // allows users to stay logged in across ages 
 const MongoStore = require('connect-mongoose');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); // loads variable from .env
 const path = require('path');
 
 dotenv.config();
@@ -17,4 +17,26 @@ mongoose.connect(process.env.MONGO_URI)
 
 //middleware
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// renders .ejs files with embedded JS in HTML
+app.set('veiw engine', 'ejs'); 
+
+// Session
+app.use(session({
+    secret: process.env.SESSION_SECRET, //ID Cookie
+    resave: false, // avoids saving if nothing changes
+    saveUninitialized: false, // avoids saving empty sessions
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }), // saves sessions to database
+    cookie: { maxAge: 1000 * 60 * 24 } //session expires after 1day
+}));
+
+// routes
+app.use('/', require('./routes/main'));
+
+// start server 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT`));
+
 
