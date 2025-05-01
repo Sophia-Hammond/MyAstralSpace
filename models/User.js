1cde7872565a67fd7ae134920c6080f54fd0812f
Sophia-Hammond <beautyinbooty@gmail.com>
2025-05-01 21:29:41 +0100

    re-writing user.js with neccesary logic

--- models/User.js
+++ models/User.js
@@ -1,35 +1,17 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
-  username: {
+const UserSchema = new mongoose.Schema({
+  email {
     type: String,
-    required: true,
+    require: true,
     unique: true,
-  },
-
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+    lowercase: true, 
+    trim: true
   },
 
   password: {
     type: String,
-    required: true,
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now,
+    required: true
   }
 });
-
-// Hash the password before saving the user
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
-
-module.exports = mongoose.model('User', userSchema);
